bc6fb940b22c85346dc7d260049a322314abb662
Astha meena <116489120+10astha@users.noreply.github.com>
2023-10-10 21:38:53 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,23 +1,4 @@
-#!/bin/bash
-
-# Check if the correct number of command line arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <input_filename>"
-    exit 1
-fi
-
-input_prefix="$1"
-input_filename="$input_prefix.graph"
-satinput_filename="$input_prefix.satinput"
-
-# Check if the input file exists
-if [ ! -f "$input_filename" ]; then
-    echo "Input file '$input_filename' not found."
-    exit 1
-fi
-
-# generate SAT input file
-g++ main.cpp ai_new_1.cpp -o SAT_inputfile_generator
-./SAT_inputfile_generator $input_filename $satinput_filename
-
-echo "SAT input file '$satinput_filename' generated."
+chmod 777 a1
+INPUT="$1.graph"
+OUPUT="$1.satinput"
+./a1 < $INPUT > $OUPUT
