ca33b8ca94e2435a55749cabfb678ec47b3f8e3e
Astha meena <116489120+10astha@users.noreply.github.com>
2023-12-28 19:27:17 +0530

    Update ai_out.cpp

--- ai_out.cpp
+++ ai_out.cpp
@@ -1,11 +1,13 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main(int argc, char *argv[]) {
     // Specify the value of n
 
     int n,E,k1,k2;
-    string inputfilename ("test.graph");
+    string filename = argv[1];
+    string inputfilename = filename+".graph";
+    // string inputfilename ("test.graph");
     fstream ipfile;
     ipfile.open(inputfilename, ios::in);
     if (!ipfile) {
@@ -22,7 +24,7 @@ int main() {
 
     stringstream G1;
     stringstream G2;
-    string inputfilename1 ("test.satoutput");
+    string inputfilename1 = filename+".satoutput";
     fstream opfile;
     opfile.open(inputfilename1, ios::in);
     if (!opfile) {
@@ -38,21 +40,31 @@ int main() {
             G1<< "#1"<<endl;
        G2<<"#2"<<endl;
             int var;
+            // cout << "k1: " << k1 << " k2: " << k2 << endl;
+            int kk1=1;
             for(int i = 1; i <= n; i++){
                 opfile >> var;
-                int kk1=0;
-                if (var > 0 and kk1<=k1) {
+                
+                if (var > 0 and kk1<k1) {
+                    // cout << "Var: " << var << endl;
                     G1 << var << " ";
+                    kk1++;
+                } else if (var > 0 and kk1==k1) {
+                    G1 << var;
                     kk1++;
                 }
                 
             }
-           
+            int kk2=1;
             for(int i=n+1;i<=2*n;i++){
                 opfile >> var;
-                int kk2=0;
-                if (var > 0 and kk2<=k2) {
+                
+                if (var > 0 and kk2<k2) {
+                    // cout << "Var: " << var << endl;
                     G2 << var-n << " ";
+                    kk2++;
+                } else if (var > 0 and kk2==k2) {
+                    G2 << var-n;
                     kk2++;
                 }
             }
@@ -63,9 +75,7 @@ int main() {
        
 
     }
-    // cout << G1.str() << endl;
-    // cout << G2.str() << endl;
-    string outputfilename ("test.mapping");
+    string outputfilename = filename+".mapping";
     ofstream outputFile(outputfilename);
 
     // Check if the file is opened successfully
@@ -75,5 +85,5 @@ int main() {
     }
 
     outputFile << G1.str()<<endl;
-    outputFile << G2.str() << endl;
+    outputFile << G2.str()<< endl;
 }
